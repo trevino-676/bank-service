699bd5d347670bdfde6d1dac21a40c14c3136344
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-10-02 01:49:50 -0500

    Se agregaron nuevos archivos para ser ignorados

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
 from config import DOTENV_PATH
+from app.routes import router
 
 
 load_dotenv(DOTENV_PATH)
@@ -15,7 +16,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+app.include_router(router)
 
 @app.get("/", tags=["root"])
 def hello():
